b618b624b536b264ed4c196b95b78034a8406d52
Keyto <keyto1995@outlook.com>
2022-11-22 14:57:09 +0800

    feat: 采用LOG;

--- main/main.c
+++ main/main.c
@@ -1,17 +1,23 @@
 #include <stdio.h>
+#include "esp_log.h"
+
+const char *tagTraffic = "交通灯";
+
+bool secretLog = true;
 
 void app_main(void)
 {
-  printf("Hello: %s \n", "2022/11/21");
-  printf("打印: %4d年%02d月%02d日 %02d时%02d分\n", 2022, 11, 21, 8, 23);
-  printf("打印%%: %5.2f%%\n", 75.1);
-  printf("temp: %f\n", 1.23);
-  printf("temp: %.1f\n", 1.23);
-  printf("指定长度: %10.1f\n", 1.23);
-  printf("指定填充字符0: %010.1f\n", 1.23);
+  if (secretLog)
+  {
+    esp_log_level_set(tagTraffic, ESP_LOG_VERBOSE);
+  }
+  ESP_LOGE(tagTraffic, "不亮了");
 
-  printf("打印符号: %+.1f\n", 1.23);
-  printf("打印符号: %+.1f\n", -1.23);
-  // 清空标准输出，与\n同作用
-  fflush(stdout);
+  ESP_LOGW(tagTraffic, "寿命还剩%i%%", 5);
+
+  ESP_LOGI(tagTraffic, "亮绿灯");
+
+  ESP_LOGD(tagTraffic, "DEBUG");
+
+  ESP_LOGV(tagTraffic, "VERBOSE");
 }
