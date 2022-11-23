be0ea9411c34ccadae8e945c65d80ab95c5ed2b7
Keyto <keyto1995@outlook.com>
2022-11-23 13:59:41 +0800

    feat: 实现大循环;

--- main/main.c
+++ main/main.c
@@ -1,23 +1,15 @@
 #include <stdio.h>
 #include "esp_log.h"
-
-const char *tagTraffic = "交通灯";
-
-bool secretLog = true;
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
 void app_main(void)
 {
-  if (secretLog)
+  int counter = 0;
+  while (true)
   {
-    esp_log_level_set(tagTraffic, ESP_LOG_VERBOSE);
+    ESP_LOGI("counter", "%d", counter);
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+    counter++;
   }
-  ESP_LOGE(tagTraffic, "不亮了");
-
-  ESP_LOGW(tagTraffic, "寿命还剩%i%%", 5);
-
-  ESP_LOGI(tagTraffic, "亮绿灯");
-
-  ESP_LOGD(tagTraffic, "DEBUG");
-
-  ESP_LOGV(tagTraffic, "VERBOSE");
 }
