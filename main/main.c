66752a37cad21e32ea88a94f2fa1b6db85933b02
Keyto <keyto1995@outlook.com>
2022-11-26 09:12:59 +0800

    feat: 点个灯;

--- main/main.c
+++ main/main.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
 #include "esp_log.h"
+#include "driver/gpio.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
+#define LED_PIN GPIO_NUM_2
+
+uint32_t status;
+
 void app_main(void)
 {
+  gpio_reset_pin(LED_PIN);
+  gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);
   int counter = 0;
   while (true)
   {
+    status = !status;
+    gpio_set_level(LED_PIN, status);
+
     ESP_LOGI("counter", "%d", counter);
     vTaskDelay(1000 / portTICK_PERIOD_MS);
     counter++;
